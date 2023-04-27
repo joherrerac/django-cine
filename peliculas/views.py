7fb1371d6fd7c6a8016ccda8b5e915bf83c9ff2d
joherrerac <jo.herrerac@duocuc.cl>
2023-04-27 17:26:38 -0400

    avance funcionalidad agregar pelicula(sin t)

--- peliculas/views.py
+++ peliculas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Pelicula
 
 # Create your views here.
@@ -6,3 +6,14 @@ from .models import Pelicula
 def home(request):
     peliculas = Pelicula.objects.all()
     return render(request, "gestionpeliculas.html", {"peliculas":peliculas})
+
+def registrarPelicula(request):
+    id=request.POST['txtId']
+    nombre=request.POST['txtNombre']
+    duracion=request.POST['txtDuracion']
+    productora=request.POST['txtProductora']
+    fecha_estreno=request.POST['txtDate']
+    
+    pelicula=Pelicula.objects.create(
+        id=id, nombre=nombre, duracion=duracion, productora=productora, fecha_estreno=fecha_estreno)
+    return redirect('/')
